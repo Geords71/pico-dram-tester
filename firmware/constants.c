63ec92b3d39e1983edc41796f761e5198479b903
acky <acky6012@gmail.com>
2025-10-27 23:47:18 +1300

    Noodling with constants.

---
+++ firmware/constants.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+const uint8_t BASE_PIN = 5;
+const uint8_t TOTAL_PIN_COUNT = 17;
+const uint8_t OUTPUT_BASE_PIN = BASE_PIN;
+const uint8_t OUTPUT_PIN_COUNT = 13;
+const uint8_t INPUT_BASE_PIN = BASE_PIN + 16;
+const uint8_t INPUT_PIN_COUNT = 1;
---
+++ firmware/constants.h
@@ -0,0 +1,9 @@
+#ifndef constants_h
+#define constants_h
+extern const uint8_t BASE_PIN;
+extern const uint8_t TOTAL_PIN_COUNT;
+extern const uint8_t OUTPUT_BASE_PIN;
+extern const uint8_t OUTPUT_PIN_COUNT;
+extern const uint8_t INPUT_BASE_PIN;
+extern const uint8_t INPUT_PIN_COUNT;
+#endif
